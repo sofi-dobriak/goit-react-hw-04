bc73b4084e8e0878d92cd19cdb4df16babbdbd34
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-04-02 14:44:56 +0300

    fix hover  styles for images

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,9 +2,9 @@ import styles from './ImageCard.module.css';
 
 const ImageCard = ({ image, onClick }) => {
     return (
-        <div onClick={onClick}>
+        <li onClick={onClick} className={styles.imageItem}>
             <img className={styles.image} src={image.urls.small} alt={image.alt_description} />
-        </div>
+        </li>
     );
 };
 
